63d9643b77cad45a6695693dd12ec6cd680618f0
Jeong Yeon Cho <chodr@bc.edu>
2019-04-05 18:45:15 -0400

    route handler for get request params

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express()
 const port = process.argv[2]
 
 
+app.get("/", (req, res) => {
+  let delay = req.query.delay
+
+  if (delay > 10000) {
+    res.send("Delay can't exeed 10 seconds")
+  } else {
+    setTimeout(() => res.send(`This reponse was sent with a delay of ${delay / 1000} seconds`), delay)
+  }
+})
+
 app.get("/:delay?", (req, res) => {
   let delay = req.params.delay
 
